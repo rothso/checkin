273f46175d0bedd88b3923c2c36cbac5b8075588
Rothanak So <rothanak.hello@gmail.com>
2019-06-19 14:31:19 -0400

    Add new splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -19,6 +20,15 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        textTheme: textTheme.copyWith(
+            headline: textTheme.headline.copyWith(
+              fontFamily: 'Jura',
+              fontWeight: FontWeight.w600,
+              fontSize: 25.0,
+            ),
+            caption: textTheme.caption.copyWith(
+              fontWeight: FontWeight.w300,
+            )),
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -66,128 +76,159 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
 
-    return Center(
-      child: Container(
-        color: Colors.white,
-        alignment: AlignmentDirectional(0.0, -0.7),
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 80.0, horizontal: 40.0),
-          child: Column(children: [
-            Image.asset("assets/hospital.png", scale: 3),
-            SizedBox(height: 40.0),
-            RichText(
-              text: TextSpan(
-                // Note: Styles for TextSpans must be explicitly defined.
-                // Child text spans will inherit styles from parent
-                style: TextStyle(fontSize: 12.0, color: Color(0xFF666666)),
-                children: [
-                  TextSpan(
-                    text: 'You are checking into ',
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                  TextSpan(
-                    text: 'West Haven Clinic',
-                    style: TextStyle(fontWeight: FontWeight.w500),
-                  ),
-                ],
-              ),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SecondRoute(),
             ),
-            SizedBox(height: 15.0),
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFF3096D6),
-                borderRadius: BorderRadius.circular(50.0),
+          ),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/trifidmedwhite.png', scale: 1.5),
+              SizedBox(height: 8.0),
+              Text(
+                'Trifid Med',
+                style: Theme.of(context).textTheme.headline,
               ),
-              padding: EdgeInsets.all(10.0),
-              child: Center(
-                child: Text(
-                  "7HAT46",
-                  style: new TextStyle(
-                    color: Colors.white,
-                    letterSpacing: 6.0,
-                    decoration: TextDecoration.none,
-                    fontWeight: FontWeight.normal,
-                    fontFamily: "Jura",
-                    fontSize: 25,
-                  ),
-                ),
+              SizedBox(height: 10.0),
+              Text(
+                'Patient check-in, simplified.',
+                style: Theme.of(context).textTheme.caption,
               ),
-            ),
-            SizedBox(height: 35.0),
-            Text(
-              "Who is the appointment for?",
-              style: TextStyle(
-                fontSize: 12.0,
-                color: Color(0xFF666666),
-                decoration: TextDecoration.none,
-                fontStyle: FontStyle.italic,
-                fontFamily: "Open Sans",
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: List.generate(
-                  3,
-                  (index) => Expanded(
-                        child: Column(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Color(0xFFC4C4C4),
-                                shape: BoxShape.circle,
-                              ),
-                              child: SizedBox(width: 60, height: 60),
-                            ),
-                            SizedBox(height: 15),
-                            Text(
-                              "Johnathan",
-                              style: TextStyle(
-                                fontSize: 12.0,
-                                color: Color(0xFF000000),
-                                decoration: TextDecoration.none,
-                                fontFamily: "Open Sans",
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10.0),
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondRoute()),
-                );
-              },
-              color: Color(0xFF343434),
-              shape: RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(50.0),
-              ),
-              child: Center(
-                child: Text(
-                  "This appointment is for someone new",
-                  style: new TextStyle(
-                    color: Colors.white,
-                    decoration: TextDecoration.none,
-                    fontWeight: FontWeight.w300,
-                    fontFamily: "Open Sans",
-                    fontSize: 12,
-                  ),
-                ),
-              ),
-            ),
-          ]),
+            ],
+          ),
         ),
       ),
     );
+
+    // return Scaffold(
+    //   body: Center(
+    //     child: Container(
+    //       color: Colors.white,
+    //       alignment: AlignmentDirectional(0.0, -0.7),
+    //       child: Container(
+    //         margin: EdgeInsets.symmetric(vertical: 80.0, horizontal: 40.0),
+    //         child: Column(children: [
+    //           Image.asset("assets/hospital.png", scale: 3),
+    //           SizedBox(height: 40.0),
+    //           RichText(
+    //             text: TextSpan(
+    //               // Note: Styles for TextSpans must be explicitly defined.
+    //               // Child text spans will inherit styles from parent
+    //               style: TextStyle(fontSize: 12.0, color: Color(0xFF666666)),
+    //               children: [
+    //                 TextSpan(
+    //                   text: 'You are checking into ',
+    //                   style: TextStyle(fontStyle: FontStyle.italic),
+    //                 ),
+    //                 TextSpan(
+    //                   text: 'West Haven Clinic',
+    //                   style: TextStyle(fontWeight: FontWeight.w500),
+    //                 ),
+    //               ],
+    //             ),
+    //           ),
+    //           SizedBox(height: 15.0),
+    //           Container(
+    //             decoration: BoxDecoration(
+    //               color: Color(0xFF3096D6),
+    //               borderRadius: BorderRadius.circular(50.0),
+    //             ),
+    //             padding: EdgeInsets.all(10.0),
+    //             child: Center(
+    //               child: Text(
+    //                 "7HAT46",
+    //                 style: new TextStyle(
+    //                   color: Colors.white,
+    //                   letterSpacing: 6.0,
+    //                   decoration: TextDecoration.none,
+    //                   fontWeight: FontWeight.normal,
+    //                   fontFamily: "Jura",
+    //                   fontSize: 25,
+    //                 ),
+    //               ),
+    //             ),
+    //           ),
+    //           SizedBox(height: 35.0),
+    //           Text(
+    //             "Who is the appointment for?",
+    //             style: TextStyle(
+    //               fontSize: 12.0,
+    //               color: Color(0xFF666666),
+    //               decoration: TextDecoration.none,
+    //               fontStyle: FontStyle.italic,
+    //               fontFamily: "Open Sans",
+    //               fontWeight: FontWeight.w400,
+    //             ),
+    //           ),
+    //           SizedBox(height: 20.0),
+    //           Container(
+    //             margin: EdgeInsets.symmetric(horizontal: 20.0),
+    //             child: Row(
+    //               mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //               children: List.generate(
+    //                 3,
+    //                 (index) => Expanded(
+    //                       child: Column(
+    //                         children: [
+    //                           Container(
+    //                             decoration: BoxDecoration(
+    //                               color: Color(0xFFC4C4C4),
+    //                               shape: BoxShape.circle,
+    //                             ),
+    //                             child: SizedBox(width: 60, height: 60),
+    //                           ),
+    //                           SizedBox(height: 15),
+    //                           Text(
+    //                             "Johnathan",
+    //                             style: TextStyle(
+    //                               fontSize: 12.0,
+    //                               color: Color(0xFF000000),
+    //                               decoration: TextDecoration.none,
+    //                               fontFamily: "Open Sans",
+    //                               fontWeight: FontWeight.normal,
+    //                             ),
+    //                           ),
+    //                         ],
+    //                       ),
+    //                     ),
+    //               ),
+    //             ),
+    //           ),
+    //           SizedBox(height: 10.0),
+    //           RaisedButton(
+    //             onPressed: () {
+    //               Navigator.push(
+    //                 context,
+    //                 MaterialPageRoute(builder: (context) => SecondRoute()),
+    //               );
+    //             },
+    //             color: Color(0xFF343434),
+    //             shape: RoundedRectangleBorder(
+    //               borderRadius: new BorderRadius.circular(50.0),
+    //             ),
+    //             child: Center(
+    //               child: Text(
+    //                 "This appointment is for someone new",
+    //                 style: new TextStyle(
+    //                   color: Colors.white,
+    //                   decoration: TextDecoration.none,
+    //                   fontWeight: FontWeight.w300,
+    //                   fontFamily: "Open Sans",
+    //                   fontSize: 12,
+    //                 ),
+    //               ),
+    //             ),
+    //           ),
+    //         ]),
+    //       ),
+    //     ),
+    //   ),
+    // );
 
     // return Scaffold(
     //   appBar: AppBar(
